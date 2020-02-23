5b6db5e28401b3959273cab064333b77ba1e504b
Arkadii Drobot <arkad242@gmail.com>
2020-02-23 23:07:34 +0200

    Add Folders and created new Project for 4 parts for UDemy course

---
+++ LearningCSharp_Block4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningCSharp_Block4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
