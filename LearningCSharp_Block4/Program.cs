7940decf4c31796bfc9bccdd56c512c37e436ce2
bzv <bzv@WIN-GEVMI51H1J2>
2020-03-05 09:58:24 +0200

    ДОбавлен урок по List

--- LearningCSharp_Block4/Program.cs
+++ LearningCSharp_Block4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LearningCSharp_Block4
 {
@@ -7,9 +9,17 @@ namespace LearningCSharp_Block4
         static void Main(string[] args)
         {
 
+
+
+
+
+        }
+
+        public void ArrayType()
+        {
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             //быстрый поиск
-            int index = Array.BinarySearch(numbers, 7); 
+            int index = Array.BinarySearch(numbers, 7);
             Console.WriteLine(index);
 
             //копирование элементов массива в массив. С использованием метода класса
@@ -22,7 +32,7 @@ namespace LearningCSharp_Block4
 
             //реверсирование
             Array.Reverse(copy);
-            foreach(var c in copy)
+            foreach (var c in copy)
             {
                 Console.WriteLine(c);
             }
@@ -44,15 +54,62 @@ namespace LearningCSharp_Block4
 
 
             //две аналогичные строчки создания листа
-            Array myarr = new int [5];
+            Array myarr = new int[5];
             Array myarr2 = Array.CreateInstance(typeof(int), 5);
 
             myarr.SetValue(12, 0);
 
             Console.WriteLine(myarr.GetValue(0));
-            
-
-
         }
+
+        public void ListType()
+        {
+            var intList = new List<int>() { 1, 2, 4, 5, 6, 7, 8, 12 };
+            intList.Add(7);
+
+            int[] intArray = { 1, 2, 3 };
+            intList.AddRange(intArray);
+
+
+            if (intList.Remove(1)) //первое попадание(удалению по значению)
+            {
+                //do
+
+
+            }
+            else { }
+
+            intList.RemoveAt(0); //Удаление по индексу
+
+            intList.Reverse();
+
+            bool containse = intList.Contains(3); //определить есть ли этот элемент в листе
+
+            int min = intList.Min();
+            int max = intList.Max();
+
+            Console.WriteLine($"MIn = {min}, Max = {max}");
+
+            int indexOf = intList.IndexOf(2); //найти индекс первой попавшейся двойки
+            int lastIndexOf = intList.LastIndexOf(2); // найти индекс последней двойки
+
+            Console.WriteLine($"indexof2 = {indexOf}, LastIndexOf2 = {lastIndexOf}");
+
+            for (int i = 0; i < intList.Count; i++)
+            {
+                Console.Write($"{intList[i]}");
+            }
+            Console.WriteLine();
+
+            foreach (int item in intList)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.ReadLine();
+        }
+
     }
+
+
+
 }
